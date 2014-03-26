654ca075e2c3cbee6de19f383de4a33082c3c007
menewol-lp <wolfgang@menezes.at>
2014-03-26 15:23:20 +0100

    commit

--- punkte/punkte/Form1.cs
+++ punkte/punkte/Form1.cs
@@ -24,9 +24,13 @@ namespace punkte
         {
             Graphics g = e.Graphics;
             SolidBrush sb = new SolidBrush(Color.Red);
-
+            Pen p = new Pen(Color.Yellow, 3);
             try
             {
+                g.DrawLine(p, panel1.Width / 3, panel1.Height / 5, panel1.Width / 3 * 2, panel1.Height / 5);
+                g.DrawLine(p, panel1.Width / 3, panel1.Height / 5, panel1.Width / 3, panel1.Height / 5 * 3);
+                g.DrawLine(p, panel1.Width / 3, panel1.Height / 5 * 3, panel1.Width / 3 * 2, panel1.Height / 5 * 3);
+
                 foreach (Point item in people)
                 {
                     g.FillEllipse(sb, item.X, item.Y, 5, 5);        
@@ -49,10 +53,20 @@ namespace punkte
             listBox1.Items.Clear();
             people.Clear();
             int anz = Convert.ToInt32(textBox1.Text);
+            int maxW = panel1.Width;
+            int maxH = panel1.Height;
+            int startW = 0;
+            int startH = 0;
+
+            if (checkBox1.Checked)
+            { 
             
+            }
+
             for (int i = 0; i < anz; i++)
             {
-                people.Add(new Point(rnd.Next(0, panel1.Width), rnd.Next(0, panel1.Height)));
+                people.Add(new Point(rnd.Next(startW, maxW),
+                                     rnd.Next(startH, maxH)));
             }
 
             foreach (Point item in people)
