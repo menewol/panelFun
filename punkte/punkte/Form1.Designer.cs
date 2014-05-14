af76310a74c03fa6163fe9b25b5ea97034433f18
menewol-home <wolfgang@menezes.at>
2014-05-15 01:24:51 +0200

    bl

--- punkte/punkte/Form1.Designer.cs
+++ punkte/punkte/Form1.Designer.cs
@@ -28,11 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // button1
@@ -88,11 +91,26 @@
             this.checkBox1.TabIndex = 4;
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(754, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(838, 478);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.panel1);
@@ -113,6 +131,8 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- punkte/punkte/Form1.cs
+++ punkte/punkte/Form1.cs
@@ -13,9 +13,12 @@ namespace punkte
     public partial class Form1 : Form
     {
         List<Point> people = new List<Point>();
+        List<Point> passingPoints = new List<Point>();
         Random rnd = new Random();
+        Point move = new Point(1, 1);
         List<Rectangle> rectLst = new List<Rectangle>();
         Point ziel;
+        bool run = false;
         
         public Form1()
         {
@@ -29,11 +32,12 @@ namespace punkte
             SolidBrush sbZiel = new SolidBrush(Color.Yellow);
             Pen p = new Pen(Color.Yellow, 3);
 
-            g.DrawLine(p, panel1.Width / 3, panel1.Height / 5, panel1.Width / 3 * 2, panel1.Height / 5);
             g.DrawLine(p, panel1.Width / 3, panel1.Height / 5, panel1.Width / 3, panel1.Height / 5 * 3);
+            g.DrawLine(p, panel1.Width / 3, panel1.Height / 5, panel1.Width / 3 * 2, panel1.Height / 5);
+            passingPoints.Add(new Point(panel1.Width / 3 * 2, panel1.Height / 5));            
             g.DrawLine(p, panel1.Width / 3, panel1.Height / 5 * 3, panel1.Width / 3 * 2, panel1.Height / 5 * 3);
-
-            
+            passingPoints.Add(new Point(panel1.Width / 3 * 2, panel1.Height / 5 * 3));
+           
             g.FillEllipse(sbZiel, ziel.X, ziel.Y, 5, 5);  
 
             Rectangle tempRect = new Rectangle(panel1.Width / 3, panel1.Height / 5, (panel1.Width / 3 * 2) - (panel1.Width / 3), (panel1.Height / 5 * 3) - (panel1.Height / 5));
@@ -109,6 +113,51 @@ namespace punkte
         private void Form1_Load(object sender, EventArgs e)
         {
             ziel = new Point(rnd.Next(0, panel1.Width), rnd.Next(0, panel1.Height));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            switch (run)
+            {
+                case false:
+                    timer1.Start();
+                    run = true;
+                    break;
+                case true:
+                    timer1.Stop();
+                    run = false;
+                    break;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            List<Point> temp = people;
+            foreach (Point item in temp)
+            {
+                Point tempHold = new Point(0, 0);
+                    if (item.X > ziel.X)
+                    {
+                        int z = people.IndexOf(item);
+                        people[z] = tempHold = new Point(item.X - 1, item.Y);
+                    }
+                    if (item.X < ziel.X)
+                    {
+                        int z = people.IndexOf(item);
+                        people[z] = tempHold = new Point(item.X + 1, item.Y);
+                    }
+               
+                    if (item.Y > ziel.Y)
+                    {
+                        int z = people.IndexOf(tempHold);
+                        people[z] = new Point(item.X, item.Y-1);
+                    }
+                    if (item.Y < ziel.Y)
+                    {
+                        int z = people.IndexOf(tempHold);
+                        people[z] = new Point(item.X, item.Y+1);
+                    }
+            }
         }
     }
 }
